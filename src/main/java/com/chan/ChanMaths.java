4fa7f0c09b251e4c71cbf5f38ec7fcd9e36bc87d
KangChanSong <thdrkdcks@naver.com>
2024-05-03 16:07:57 +0900

    [ADD] most initial commit

---
+++ src/main/java/com/chan/ChanMaths.java
@@ -0,0 +1,8 @@
+package com.chan;
+
+public interface ChanMaths {
+    int plus(int x, int y);
+    int minus(int x, int y);
+    int multiply(int x, int y);
+    int divide(int x, int y);
+}
---
+++ src/main/java/com/chan/ChanMathsImpl.java
@@ -0,0 +1,23 @@
+package com.chan;
+
+public class ChanMathsImpl implements ChanMaths{
+    @Override
+    public int plus(int x, int y) {
+        return x + y;
+    }
+
+    @Override
+    public int minus(int x, int y) {
+        return x - y;
+    }
+
+    @Override
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+
+    @Override
+    public int divide(int x, int y) {
+        return x / y;
+    }
+}
